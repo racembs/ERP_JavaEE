d5bc11e801be603133d704a557723342eda8bd6c
Asus <Asus@DESKTOP-LBIF6G4>
2018-03-17 22:10:52 +0100

    page authentification avec redirection role vers deux pages
    Signed-off-by: mehdi <mehdi.benahmed@esprit.tn>

---
+++ esprit1718b4eventmanagement/esprit1718b4eventmanagement-client/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/app/client/test/testt.java
@@ -0,0 +1,28 @@
+package tn.esprit.b4.esprit1718b4eventmanagement.app.client.test;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class testt extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent parent =FXMLLoader.load(getClass().getResource("/views/login.fxml"));
+		Scene scene=new Scene(parent);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+ 
+}
--- esprit1718b4eventmanagement/esprit1718b4eventmanagement-service/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/services/UserServiceLocal.java
+++ esprit1718b4eventmanagement/esprit1718b4eventmanagement-service/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/services/UserServiceLocal.java
@@ -1,6 +1,7 @@
 package tn.esprit.b4.esprit1718b4eventmanagement.services;
 
 import javax.ejb.Local;
+import java.util.List;
 
 import tn.esprit.b4.esprit1718b4eventmanagement.entities.User;
 import tn.esprit.b4.esprit1718b4eventmanagement.utilities.IGenericDAO;
@@ -9,5 +10,11 @@ import tn.esprit.b4.esprit1718b4eventmanagement.utilities.IGenericDAO;
 public interface UserServiceLocal extends IGenericDAO<User> {
 
 	User login(String login, String password);
+	public User add(User u);
+	public User update(User u);
+	public Boolean remove(Integer id);
+	public List<User> findAll();
+	public User findById(Integer id);
+	public User findByLogin(String l);
 
 }
