b698e8b0048009111f77248a15f95bf9f315911c
Asus <Asus@DESKTOP-LBIF6G4>
2018-03-23 23:36:50 +0100

    Signed-off-by: Asus <Asus@DESKTOP-LBIF6G4>

--- esprit1718b4eventmanagement/esprit1718b4eventmanagement-service/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/services/UserServiceLocal.java
+++ esprit1718b4eventmanagement/esprit1718b4eventmanagement-service/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/services/UserServiceLocal.java
@@ -10,7 +10,9 @@ import tn.esprit.b4.esprit1718b4eventmanagement.utilities.IGenericDAO;
 public interface UserServiceLocal extends IGenericDAO<User> {
 
 	User login(String login, String password);
-
+	public List<User> SearchFirstName(String login);
+	public List<User> SearchMail(String login);
+	public List<User> SearchStatut(String sta);
 	public void update1(User u);
 	public List<User> SearchLogin(String login);
 	public User findByLogin(String l);
