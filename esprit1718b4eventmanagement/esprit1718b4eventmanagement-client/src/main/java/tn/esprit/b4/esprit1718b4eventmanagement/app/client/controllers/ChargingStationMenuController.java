4efbd020129414c6a28eaed7976e032a33b7a0c7
ons <ons.cherif@esprit.tn>
2018-04-08 16:45:00 +0200

    API Calendar

--- esprit1718b4eventmanagement/esprit1718b4eventmanagement-client/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/app/client/controllers/ChargingStationMenuController.java
+++ esprit1718b4eventmanagement/esprit1718b4eventmanagement-client/src/main/java/tn/esprit/b4/esprit1718b4eventmanagement/app/client/controllers/ChargingStationMenuController.java
@@ -9,18 +9,21 @@ import java.util.ResourceBundle;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
-
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.collections.ListChangeListener;
 import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Separator;
 import javafx.scene.control.TreeItem.TreeModificationEvent;
 import javafx.scene.control.TreeView;
@@ -31,6 +34,9 @@ import org.apache.commons.logging.LogFactory;
 import org.controlsfx.control.CheckTreeView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.scene.control.CheckBoxTreeItem;
 import javafx.scene.control.TreeItem;
@@ -264,118 +270,171 @@ public class ChargingStationMenuController implements Initializable {
     	idCal.setOnMouseClicked((MouseEvent e) -> { 
 			  
 			  System.out.println("Calendar Clicked!"); // change functionality
-	            Parent parent= null;
-	            
+			  
+			  Parent parent= null;
+			  	try {
+			  		
+			  		VBox vb = new VBox();
+	  				parent  =FXMLLoader.load(getClass().getResource("/views/Stat.fxml"));
+	  				
+	  				Stage primaryStage= new Stage(); 
+	  			
+	  				idcancel.getScene().getWindow().hide();
+	  				
+	  			    vb.setId("root");
+
+	  			    WebView  browser = new WebView();
+	  			    WebEngine engine = browser.getEngine();
+	  			    String url1 = "https://calendar.google.com/calendar/embed?src=gpaoons%40gmail.com&ctz=Africa%2FTunis";
+	  			    engine.load(url1);
+	  			    
+	  			    vb.setPadding(new Insets(30, 50, 50, 50));
+	  			    vb.setSpacing(10);
+	  			    vb.setAlignment(Pos.CENTER);
+	  			    vb.getChildren().addAll(browser);
+
+	  			    Scene scene = new Scene(vb);
+	  			    primaryStage.setScene(scene);
+	  			    primaryStage.show();
+			  	} catch (Exception e1) {
+	  				// TODO Auto-generated catch block
+	  				e1.printStackTrace();
+	  			}
+			  	
+			  	
+//	            Parent parent= null;
+//	            Stage stage = null;
+//	            stage.setTitle("HTML");
+//	            stage.setWidth(500);
+//	            stage.setHeight(500);
+//	            Scene scene = new Scene(new Group());
+//
+//	            VBox root = new VBox();     
+//
+//	            final WebView browser = new WebView();
+//	            final WebEngine webEngine = browser.getEngine();
+//
+//	            ScrollPane scrollPane = new ScrollPane();
+//	            scrollPane.setContent(browser);
+//	            webEngine.loadContent("https://calendar.google.com/calendar/embed?src=gpaoons%40gmail.com&ctz=Africa%2FTunis");
+//
+//	            root.getChildren().addAll(scrollPane);
+//	            scene.setRoot(root);
+//
+//	            stage.setScene(scene);
+//	            stage.show();      
+	        
+	                  
 	            /////////////////////////////////////////////////
-	            CalendarController quickstart =new CalendarController();
-	            
-				
-		
-	            try {
-	            	 // Build a new authorized API client service.
-	                // Note: Do not confuse this class with the
-	                //   com.google.api.services.calendar.model.Calendar class.
-	                Calendar service;
-					try {
-						service = quickstart.getCalendarService();
-						 // List the next 10 events from the primary calendar.
-		                DateTime now = new DateTime(System.currentTimeMillis());
-		                Events events;
-						
-							events = service.events().list("primary")
-							    .setMaxResults(10)
-							    .setTimeMin(now)
-							    .setOrderBy("startTime")
-							    .setSingleEvents(true)
-							    .execute();
-							
-			                List<Event> items = events.getItems();
-			                if (items.size() == 0) {
-			                    System.out.println("No upcoming events found.");
-			                } else {
-			                    System.out.println("Upcoming events");
-			                    for (Event event : items) {
-			                        DateTime start = event.getStart().getDateTime();
-			                        if (start == null) {
-			                            start = event.getStart().getDate();
-			                        }
-			                        System.out.printf("%s (%s)\n", event.getSummary(), start);
-			                        System.out.printf("%s (%s)\n", event.getId(), start);
-			                     
-			                     
-			                     /////////////////////////////////////
-			                     // Refer to the Java quickstart on how to setup the environment:
-			                     // https://developers.google.com/calendar/quickstart/java
-			                     // Change the scope to CalendarScopes.CALENDAR and delete any stored
-			                     // credentials.
-			                    	quickstart.authorize();
-			                     Event event1 = new Event()
-			                         .setSummary("Google I/O 2015")
-			                         .setLocation("800 Howard St., San Francisco, CA 94103")
-			                         .setDescription("A chance to hear more about Google's developer products.");
-
-			                     DateTime startDateTime = new DateTime("2018-05-28T09:00:00-07:00");
-			                     EventDateTime start1 = new EventDateTime()
-			                         .setDateTime(startDateTime)
-			                         .setTimeZone("America/Los_Angeles");
-			                     event1.setStart(start1);
-
-			                     DateTime endDateTime = new DateTime("2018-05-28T17:00:00-07:00");
-			                     EventDateTime end = new EventDateTime()
-			                         .setDateTime(endDateTime)
-			                         .setTimeZone("America/Los_Angeles");
-			                     event1.setEnd(end);
-
-			                     String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
-			                     event1.setRecurrence(Arrays.asList(recurrence));
-
-			                     EventAttendee[] attendees = new EventAttendee[] {
-			                         new EventAttendee().setEmail("gpaoons@gmail.com"),
-			                         new EventAttendee().setEmail("gpaoons@gmail.com"),
-			                     };
-			                     event1.setAttendees(Arrays.asList(attendees));
-
-			                     EventReminder[] reminderOverrides = new EventReminder[] {
-			                         new EventReminder().setMethod("email").setMinutes(24 * 60),
-			                         new EventReminder().setMethod("popup").setMinutes(10),
-			                     };
-			                     Event.Reminders reminders = new Event.Reminders()
-			                         .setUseDefault(false)
-			                         .setOverrides(Arrays.asList(reminderOverrides));
-			                     event1.setReminders(reminders);
-
-			                     String calendarId = "primary";
-			                     event1 = service.events().insert(calendarId, event1).execute();
-			                     System.out.printf("Event created: %s\n", event1.getHtmlLink());
-			                     ////////////////////////////////////////   
-			                     // Delete an event
-			                     //add the id
-			                        service.events().delete("primary", event.getId()).execute();
-			                     
-			                        
-			                     // Retrieve the event from the API
-			                        Event event2 = service.events().get("primary", event.getId()).execute();
-
-			                        // Make a change
-			                        event2.setSummary("Appointment at Somewhere");
-			                        
-			                     // Update the event
-			                        Event updatedEvent = service.events().update("primary", event2.getId(), event2).execute();
-			                        System.out.println(updatedEvent.getUpdated());
-			                    }
-			                }
-					
-
-	               
-					} catch (Exception e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-	
-	           
-	            }finally {
-	            	 System.out.printf("i'm very happy 😉  ");
-	    		}
+//	            CalendarController quickstart =new CalendarController();
+//	            
+//				
+//		
+//	            try {
+//	            	 // Build a new authorized API client service.
+//	                // Note: Do not confuse this class with the
+//	                //   com.google.api.services.calendar.model.Calendar class.
+//	                Calendar service;
+//					try {
+//						service = quickstart.getCalendarService();
+//						 // List the next 10 events from the primary calendar.
+//		                DateTime now = new DateTime(System.currentTimeMillis());
+//		                Events events;
+//						
+//							events = service.events().list("primary")
+//							    .setMaxResults(10)
+//							    .setTimeMin(now)
+//							    .setOrderBy("startTime")
+//							    .setSingleEvents(true)
+//							    .execute();
+//							
+//			                List<Event> items = events.getItems();
+//			                if (items.size() == 0) {
+//			                    System.out.println("No upcoming events found.");
+//			                } else {
+//			                    System.out.println("Upcoming events");
+//			                    for (Event event : items) {
+//			                        DateTime start = event.getStart().getDateTime();
+//			                        if (start == null) {
+//			                            start = event.getStart().getDate();
+//			                        }
+//			                        System.out.printf("%s (%s)\n", event.getSummary(), start);
+//			                        System.out.printf("%s (%s)\n", event.getId(), start);
+//			                     
+//			                     
+//			                     /////////////////////////////////////
+//			                     // Refer to the Java quickstart on how to setup the environment:
+//			                     // https://developers.google.com/calendar/quickstart/java
+//			                     // Change the scope to CalendarScopes.CALENDAR and delete any stored
+//			                     // credentials.
+//			                    	quickstart.authorize();
+//			                     Event event1 = new Event()
+//			                         .setSummary("Google I/O 2015")
+//			                         .setLocation("800 Howard St., San Francisco, CA 94103")
+//			                         .setDescription("A chance to hear more about Google's developer products.");
+//
+//			                     DateTime startDateTime = new DateTime("2018-05-28T09:00:00-07:00");
+//			                     EventDateTime start1 = new EventDateTime()
+//			                         .setDateTime(startDateTime)
+//			                         .setTimeZone("America/Los_Angeles");
+//			                     event1.setStart(start1);
+//
+//			                     DateTime endDateTime = new DateTime("2018-05-28T17:00:00-07:00");
+//			                     EventDateTime end = new EventDateTime()
+//			                         .setDateTime(endDateTime)
+//			                         .setTimeZone("America/Los_Angeles");
+//			                     event1.setEnd(end);
+//
+//			                     String[] recurrence = new String[] {"RRULE:FREQ=DAILY;COUNT=2"};
+//			                     event1.setRecurrence(Arrays.asList(recurrence));
+//
+//			                     EventAttendee[] attendees = new EventAttendee[] {
+//			                         new EventAttendee().setEmail("gpaoons@gmail.com"),
+//			                         new EventAttendee().setEmail("gpaoons@gmail.com"),
+//			                     };
+//			                     event1.setAttendees(Arrays.asList(attendees));
+//
+//			                     EventReminder[] reminderOverrides = new EventReminder[] {
+//			                         new EventReminder().setMethod("email").setMinutes(24 * 60),
+//			                         new EventReminder().setMethod("popup").setMinutes(10),
+//			                     };
+//			                     Event.Reminders reminders = new Event.Reminders()
+//			                         .setUseDefault(false)
+//			                         .setOverrides(Arrays.asList(reminderOverrides));
+//			                     event1.setReminders(reminders);
+//
+//			                     String calendarId = "primary";
+//			                     event1 = service.events().insert(calendarId, event1).execute();
+//			                     System.out.printf("Event created: %s\n", event1.getHtmlLink());
+//			                     ////////////////////////////////////////   
+//			                     // Delete an event
+//			                     //add the id
+//			                        service.events().delete("primary", event.getId()).execute();
+//			                     
+//			                        
+//			                     // Retrieve the event from the API
+//			                        Event event2 = service.events().get("primary", event.getId()).execute();
+//
+//			                        // Make a change
+//			                        event2.setSummary("Appointment at Somewhere");
+//			                        
+//			                     // Update the event
+//			                        Event updatedEvent = service.events().update("primary", event2.getId(), event2).execute();
+//			                        System.out.println(updatedEvent.getUpdated());
+//			                    }
+//			                }
+//					
+//
+//	               
+//					} catch (Exception e1) {
+//						// TODO Auto-generated catch block
+//						e1.printStackTrace();
+//					}
+//	
+//	           
+//	            }finally {
+//	            	 System.out.printf("i'm very happy 😉  ");
+//	    		}
 	            ////////////////////////////////////////////////////
 	            
 	            
